8e8bb9ba60620a08d98e01075ca79ad6894273a7
Matheus B. Sousa <matheusbsousa2@gmail.com>
2026-06-29 16:06:46 -0300

    Update drone_boot.lua

--- drone_boot.lua
+++ drone_boot.lua
@@ -6,10 +6,24 @@ end
 local function log(s)
   st(s) local t=tun() if t then t.send(tostring(s)) end
 end
-log("listando...")
-for addr,tipo in component.list() do
-  log(tipo)
-  computer.pullSignal(0.2)
+st("boot")
+local i=component.proxy(component.list("internet")())
+if not i then st("SEM INET") while true do computer.pullSignal(1) end end
+local h=i.request("https://raw.githubusercontent.com/MatheusBS22/oc-armour-test/main/drone.lua")
+h.finishConnect()
+local c=""
+repeat local d=h.read(512) if d then c=c..d end until not d
+h.close()
+st("bytes:"..#c)
+computer.pullSignal(0.3)
+local fn,err=load(c)
+if not fn then
+  log("SYN:"..tostring(err))
+  while true do computer.pullSignal(1) end
 end
-st("done")
-while true do computer.pullSignal(1) end
+st("run")
+local ok,e=pcall(fn)
+if not ok then
+  log("ERR:"..tostring(e))
+  while true do computer.pullSignal(1) end
+end
